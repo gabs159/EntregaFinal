8714acf3d636d19728c81028a82fb7f3f56e2bb5
Pharcon <Pharcon@192.168.0.14>
2017-04-02 16:07:43 -0300

    +

--- src/main/java/com/dev/web/mobile/impl/Logout.java
+++ src/main/java/com/dev/web/mobile/impl/Logout.java
@@ -4,25 +4,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-import com.dev.web.mobile.util.ActExec;
+import com.dev.web.mobile.interfaces.Instancia;
 import com.dev.web.mobile.util.Constantes;
-import com.dev.web.mobile.util.Utilities;
 
-public class Logout implements ActExec {
-	
-	public String execute(HttpServletRequest request, HttpServletResponse response) {
+public class Logout implements Instancia {
+
+	public String executa(HttpServletRequest request, HttpServletResponse response) {
 		HttpSession session = request.getSession();
 		session.removeAttribute("usuario.hashcode");
 		session.invalidate();
 		
-		return Utilities.buildJSPRootPath(Constantes.INDEX_JSP);
-	}
-
-	@Override
-	public String exc(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return null;
+		return Constantes.ROOT + "index.jsp";
 	}
 
 }
